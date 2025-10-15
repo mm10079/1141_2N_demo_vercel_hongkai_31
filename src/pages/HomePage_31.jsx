5e271819fd251c7f037171c30db897571da0aea3
mm10079 <mm1007993@gmail.com>
2025-10-15 20:43:37 +0800

    Update All

--- src/pages/HomePage_31.jsx
+++ src/pages/HomePage_31.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function HomePage_31() {
   return (
-    <div>
-      <h1>HomePage_31</h1>
-      <h2>黃泓凱, 214417031</h2>
+    <div className='flex flex-col justify-center item-center'>
+      <h1 className='text-2xl text-purple-700'>HomePage_31</h1>
+      <h2 className='text-xl'>黃泓凱, 214417031</h2>
     </div>
   );
 }

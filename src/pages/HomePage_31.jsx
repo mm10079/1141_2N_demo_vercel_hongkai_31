f912098f4e3a591b6cdc2983c1e501cf90d7c32b
mm10079 <mm1007993@gmail.com>
2025-10-15 20:44:46 +0800

    Fix HomePage_31

--- src/pages/HomePage_31.jsx
+++ src/pages/HomePage_31.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HomePage_31() {
   return (
-    <div className='flex flex-col justify-center item-center'>
+    <div className='flex flex-col justify-center items-center'>
       <h1 className='text-2xl text-purple-700'>HomePage_31</h1>
       <h2 className='text-xl'>黃泓凱, 214417031</h2>
     </div>
